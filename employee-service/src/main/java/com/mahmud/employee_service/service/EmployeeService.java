d42fbe8f1727664d4de64059a2f7cac2547f03a8
M. Oly Mahmud <mugdhodzs38@gmail.com>
2024-10-22 14:16:01 +0600

    fetch dept info from dept service

--- employee-service/src/main/java/com/mahmud/employee_service/service/EmployeeService.java
+++ employee-service/src/main/java/com/mahmud/employee_service/service/EmployeeService.java
@@ -1,22 +1,36 @@
 package com.mahmud.employee_service.service;
 
+import com.mahmud.employee_service.dto.Department;
+import com.mahmud.employee_service.dto.EmployeeResponse;
 import com.mahmud.employee_service.model.Employee;
 import com.mahmud.employee_service.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class EmployeeService {
     private final EmployeeRepository employeeRepository;
+    private RestTemplate restTemplate;
 
-    public EmployeeService(EmployeeRepository employeeRepository) {
+    @Autowired
+    public EmployeeService(EmployeeRepository employeeRepository, RestTemplate restTemplate) {
         this.employeeRepository = employeeRepository;
+        this.restTemplate = restTemplate;
     }
 
     public Employee addEmployee(Employee employee){
         return employeeRepository.save(employee);
     }
 
-    public Employee getEmployeeById(Long id){
-        return employeeRepository.findById(id).orElse(null);
+    public EmployeeResponse getEmployeeById(Long id){
+        Employee employee = employeeRepository.findById(id).orElse(null);
+        Department department = restTemplate.getForObject("http://DEPARTMENT-SERVICE/department/" + id, Department.class);
+        EmployeeResponse employeeResponse = new EmployeeResponse();
+        assert employee != null;
+        employeeResponse.setId(employee.getId());
+        employeeResponse.setName(employee.getName());
+        employeeResponse.setDepartment(department);
+        return employeeResponse;
     }
 }
